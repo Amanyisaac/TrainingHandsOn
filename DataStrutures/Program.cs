61a32387e9eae15e01c92775f0df1dacbb3c7cfa
Amany <amany.isaac.ezzat.22@gmail.com>
2025-05-02 12:16:36 +0300

    init folder structure

---
+++ DataStrutures/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStrutures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare a variable of the ListDS
+            //add item to listDS
+            //add item to listDS
+            //remove item from listDS
+        }
+    }
+}
