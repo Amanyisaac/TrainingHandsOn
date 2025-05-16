de432058c2b4fc20e382484e2845bc90ec906eaf
Amany <amany.isaac.ezzat.22@gmail.com>
2025-05-16 10:42:34 +0300

    linkedlist

---
+++ DataStrutures/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStrutures
+{
+    public  class Node
+    {
+        //contains (value,node)
+        // data (value stored)
+        public int Value { get; set; } //data
+        public Node next { get; set; } // (pointer) next node (data type node the same thig refer to  )
+        public Node(int value)
+        {
+            Value = value;
+            next = null;
+        }
+    }
+}
--- DataStrutures/Program.cs
+++ DataStrutures/Program.cs
@@ -1,19 +1,40 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace DataStrutures
 {
-    internal class Program
+
+    class Program
     {
         static void Main(string[] args)
         {
-            // declare a variable of the ListDS
-            //add item to listDS
-            //add item to listDS
-            //remove item from listDS
+            LinkedListDS list = new LinkedListDS();
+            list.AddLast(1);
+            list.AddLast(2);
+            list.AddLast(3);
+            list.AddLast(4);
+            list.AddLast(5);
+
+            Console.WriteLine("Linked List:");
+            list.Traverse();
+
+            Console.WriteLine("Adding node after 3:");
+            list.AddAfter(3, 6);
+            list.Traverse();
+
+            Console.WriteLine("Adding node before 3:");
+            list.AddBefore(3, 7);
+            list.Traverse();
+
+            Console.WriteLine("Removing node 3:");
+            list.Remove(3);
+            list.Traverse();
         }
     }
 }
+ 
+
+ 
