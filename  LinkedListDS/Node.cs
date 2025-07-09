6fc182a96db47b494af9353158ef399404141bce
Amany <amany.isaac.ezzat.22@gmail.com>
2025-07-09 21:06:28 +0300

    adding all fiels

---
+++  LinkedListDS/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStrutures
+{
+    public class Node
+    {
+      
+        public int Value { get; set; }   
+        public Node next { get; set; }   
+        public Node(int value)
+        {
+            Value = value;
+            next = null;
+        }
+    }
+}
