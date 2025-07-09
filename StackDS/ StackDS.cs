6b74b5f051bb40510aedfbf529cb6e93b1599557
Amany <amany.isaac.ezzat.22@gmail.com>
2025-07-09 20:58:43 +0300

    add al

---
+++ StackDS/ StackDS.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStrutures.StackDS
+{
+    public class StackDS
+    {
+        Node top;
+        public StackDS()
+        {
+            top = null;
+        }
+        public bool isEmpty()
+        {
+            if (top == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void push(int item)
+        {
+            if (isEmpty())
+            {
+                Node newnode = new Node(item);
+                newnode.Next = null;
+                top = newnode;
+
+            }
+            else
+            {
+                Node newnod = new Node(item);
+                newnod.Next = top;
+                top = newnod;
+            }
+        }
+
+        public int pop()
+        {
+            int value = top.Data;
+            Node delptr = top;
+            value = top.Data;
+            top = top.Next;
+
+            return value;
+        }
+
+
+
+        public int peek()
+        {
+            return top.Data;
+
+
+        }
+
+        public void display()
+        {
+            Node current = top;
+            while (current != null)
+            {
+                Console.WriteLine(current.Data);
+                current = current.Next;
+            }
+
+        }
+        public int count()
+        {
+            Node current = top;
+            int count = 0;
+            while (current != null)
+            {
+                count++;
+                current = current.Next;
+            }
+            return count;
+
+        }
+    }
+}
---
+++ StackDS/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using DataStrutures.StackDS;
+
+namespace StackMainApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StackDS myStack = new StackDS();
+
+
+            myStack.push(10);
+            myStack.push(20);
+            myStack.push(30);
+
+            Console.WriteLine("Current stack:");
+            myStack.display();
+
+            Console.WriteLine("Top element (peek): " + myStack.peek());
+
+
+            Console.WriteLine("Popped element: " + myStack.pop());
+
+
+            Console.WriteLine("Stack after pop:");
+            myStack.display();
+            Console.WriteLine("Number of elements in stack: " + myStack.Count());
+
+
+        }
+    }
+}
